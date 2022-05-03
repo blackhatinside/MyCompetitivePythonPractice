6b79b3a9e15c157d1bd45095d18af2a74d11060a
cyberkid05 <cyberadithya05@gmail.com>
2022-05-03 17:31:44 +0530

    Update Codechef.js

--- Submissions/Codechef.js
+++ Submissions/Codechef.js
@@ -27,3 +27,18 @@ for (let tc = 0; tc < tcs; tc++) {
     let n = parseInt(input());
     console.log(~~(n / 5) * 4 + (n % 5));   // ~~ used for Floor Division
 }
+
+// https://www.codechef.com/problems/SEMCOURSES
+let fs = require("fs");
+let data = fs.readFileSync(0, "utf-8");
+let idx = 0;
+data = data.split("\n");
+function input() {
+  return data[idx++];
+}
+
+let tcs = parseInt(input());
+for (let tc = 0; tc < tcs; tc++) {
+    let arr = input().split(" ").map(Number);
+    console.log(arr[0] * arr[1] * arr[2]);
+}
