030f7c830ac480f7ed92a8f1dffa0d3e41635e50
cyberkid05 <cyberadithya05@gmail.com>
2022-05-03 17:38:57 +0530

    Update Codechef.py

--- Submissions/Codechef.py
+++ Submissions/Codechef.py
@@ -15,3 +15,16 @@ tcs = int(input())
 for tc in range(tcs):
     arr = tuple(map(int, input().split()))
     print(arr[0] * arr[1] * arr[2])
+
+# https://www.codechef.com/problems/BIRDFARM
+tcs = int(input())
+for tc in range(tcs):
+    cx, dy, z = tuple(map(int, input().split()))
+    if z % cx == 0 and z % dy == 0:
+        print("ANY")
+    elif z % cx == 0:
+        print("CHICKEN")
+    elif z % dy == 0:
+        print("DUCK")
+    else:
+        print("NONE")
