d2a3834de36b7edf7e32056da59d11b49087eecc
cyberkid05 <cyberadithya05@gmail.com>
2022-05-03 16:48:41 +0530

    Update Codechef.py

--- Submissions/Codechef.py
+++ Submissions/Codechef.py
@@ -3,3 +3,9 @@ tcs = int(input())
 for tc in range(tcs):
     x, y = map(int, input().split())
     print("A" if x > y else "B")
+
+# https://www.codechef.com/problems/PRESENTS
+tcs = int(input())
+for tc in range(tcs):
+    n = int(input())
+    print((n // 5) * 4 + (n % 5))
